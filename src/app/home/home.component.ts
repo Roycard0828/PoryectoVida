1650497145be8f9a25bfe8af98b54c610ad32ada
Rodrigo <roygar0828@outlook.es>
2021-09-22 21:46:24 -0500

    Service finalizado #6

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -35,11 +36,13 @@ export class HomeComponent implements OnInit {
   goalText: string= 'My first life goal';
   goals = [] as any;
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit(): void {
 
+    this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
 
   }
 
@@ -47,9 +50,11 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText)
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i : number){
     this.goals.splice(i, 1);
+    this._data.changeGoal(this.goals);
   }
 }
